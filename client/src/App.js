7234d78e7445fabb80c335a2e187ef6962f9c1af
Jose Luis Koller <joseluiskoller98@gmail.com>
2022-10-18 18:37:35 -0300

    Refugio a duras penas

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./components/header/header";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Posts from "./pages/posts";
-import Refugees from "./pages/refugees";
+import Profile from "./pages/shelterDetail";
 import About from "./pages/about";
 import Footer from "./components/footer/footer";
 import NotFound from "./pages/notFound";
@@ -18,7 +18,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route path="/posts" element={<Posts />} />
           <Route path="/posts/coco" element={<AnimalDetail />} />
-          <Route path="/refugees" element={<Refugees />} />
+          <Route path="/refugees" element={<Profile />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
