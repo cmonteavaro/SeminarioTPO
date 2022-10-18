83d5084405baef2e024545e85d4cfacda0f84108
Matias Santoro <102833306+MatiSantoro@users.noreply.github.com>
2022-10-18 10:25:49 -0300

    post fixes (#14)
    Co-authored-by: MSantoro <m.santoro@redlink.com.ar>

--- client/src/components/animals/animalCards.js
+++ client/src/components/animals/animalCards.js
@@ -1,13 +1,16 @@
 import AnimalCard from "./animalCard";
+import "../../styles/grid-animals.css"
 
 export default function ListAnimals({ props }) {
   console.log(props);
   return (
-    <div className="grid-animales">
+    <div>
       <h2>Conocelos</h2>
-      {props && props.length > 0
-        ? props.map((animal) => <AnimalCard animal={animal} />)
-        : null}
+      <div className="grid-animales">
+        {props && props.length > 0
+          ? props.map((animal) => <AnimalCard animal={animal} />)
+          : null}
+      </div>
     </div>
   );
 }
