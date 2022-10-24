63bec7bd8ecf13288ad990bf2fb76b5a05eef460
Juampi <juampidieguez123@gmail.com>
2022-10-23 21:39:51 -0300

    EmailService para texto en HTML

--- server/src/main/java/ar/edu/uade/server/service/EmailServiceImpl.java
+++ server/src/main/java/ar/edu/uade/server/service/EmailServiceImpl.java
@@ -11,8 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.internet.MimeMessage;
 import java.util.Optional;
 
 @Service
@@ -23,12 +25,23 @@ public class EmailServiceImpl {
     public Boolean sendSimpleMail(String recipient, String subject, String msgBody)
     {
         try {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            // ---- Con Mime y helper
+            MimeMessage mailMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mailMessage, true);
 
             mailMessage.setFrom(sender);
-            mailMessage.setTo(recipient);
-            mailMessage.setText(msgBody);
+            mailMessage.setRecipients(MimeMessage.RecipientType.TO,recipient);
             mailMessage.setSubject(subject);
+
+            helper.setText(msgBody,true);
+
+            // ----- Con simple mail (No permite HTML)
+//            SimpleMailMessage mailMessage = new SimpleMailMessage();
+//
+//            mailMessage.setFrom(sender);
+//            mailMessage.setTo(recipient);
+//            mailMessage.setText(msgBody);
+//            mailMessage.setSubject(subject);
 
             javaMailSender.send(mailMessage);
             return true;
@@ -39,7 +52,7 @@ public class EmailServiceImpl {
 
     public Boolean sendMailDTO(FormularioDTO formularioDTO, Adopcion adopcion){
         String subject = "Solicitud de adopcion: "+adopcion.getAnimal().getNombre();
-        String msg = adopcion.getAnimal().getNombre()+" ha recibido una solicitud de adopcion por parte de "+formularioDTO.getNombrePostulante()+". \nContacto: "+formularioDTO.getTelefonoPostulante();
+        String msg = "<h1 style='color: red'> Hola! </h1> "+adopcion.getAnimal().getNombre()+" ha recibido una solicitud de adopcion por parte de "+formularioDTO.getNombrePostulante()+". \nContacto: "+formularioDTO.getTelefonoPostulante();
         return this.sendSimpleMail(adopcion.getRefugio().getUsuario(),subject,msg);
     }
 
