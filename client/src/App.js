c94542982ed1beacfa77c5c60abbea5395d65526
cmonteavaro <constantino20132@gmail.com>
2022-10-26 09:09:11 -0300

    agrego la ruta y archivo principal de voluntarios

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import "./styles/style.css";
 import NavBar from "./components/header/header";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
-import Posts from "./pages/posts";
+import AnimalPosts from "./pages/animalPosts";
 import Profile from "./pages/shelterDetail";
 import About from "./pages/about";
 import Footer from "./components/footer/footer";
 import NotFound from "./pages/notFound";
 import AnimalDetail from "./pages/animalDetail";
 import Shelters from "./pages/shelters";
+import VolunteerPosts from "./pages/volunteerPosts";
 
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
       <main>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/posts" element={<Posts />} />
+          <Route path="/volunteer_posts" element={<VolunteerPosts />} />
+          <Route path="/posts" element={<AnimalPosts />} />
           <Route path="/posts/:id" element={<AnimalDetail />} />
           <Route path="/refugees/" element={<Shelters />} />
           <Route path="/refugees/:id" element={<Profile />} />
