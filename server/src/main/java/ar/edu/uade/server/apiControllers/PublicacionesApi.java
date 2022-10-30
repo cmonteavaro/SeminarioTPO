21b8d6d65094a2b1c1d221b6f41c91c6004ebab4
Juan Dieguez <85458577+JuanDieguez@users.noreply.github.com>
2022-10-30 19:22:56 -0300

    Formulario mail - back (#31)
    * emailservice para adopciones
    
    * emailservice para transito y voluntariado
    
    * EmailService para texto en HTML
    
    * FormularioDTO correcto
    
    * Implementacion de correo en postulacion
    
    * Fix en postulacion transito
    
    * Documentacion de postulaciones y fixes

--- server/src/main/java/ar/edu/uade/server/apiControllers/PublicacionesApi.java
+++ server/src/main/java/ar/edu/uade/server/apiControllers/PublicacionesApi.java
@@ -1,6 +1,7 @@
 package ar.edu.uade.server.apiControllers;
 
 import ar.edu.uade.server.DTO.AdopcionDTO;
+import ar.edu.uade.server.DTO.FormularioDTO;
 import ar.edu.uade.server.DTO.DonacionDTO;
 import ar.edu.uade.server.DTO.VoluntarioDTO;
 import ar.edu.uade.server.exceptions.RefugioException;
@@ -15,10 +16,10 @@ import ar.edu.uade.server.service.TransitoService;
 import ar.edu.uade.server.views.*;
 import ar.edu.uade.server.model.PublicacionVoluntariado;
 import ar.edu.uade.server.service.AdopcionService;
+import ar.edu.uade.server.service.EmailServiceImpl;
 import ar.edu.uade.server.service.VoluntarioService;
 import ar.edu.uade.server.views.AdopcionView;
 import ar.edu.uade.server.views.VoluntariadoView;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,7 +28,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
@@ -37,13 +37,15 @@ public class PublicacionesApi {
     private final AdopcionService adopcionService;
     private final TransitoService transitoService;
     private final VoluntarioService voluntarioService;
+    private final EmailServiceImpl emailService;
     private final RefugioService refugioService;
     private final DonacionService donacionService;
 
     @Autowired
-    public PublicacionesApi (VoluntarioService vs, AdopcionService as, TransitoService ts, RefugioService rs, DonacionService ds){
+    public PublicacionesApi (VoluntarioService vs, AdopcionService as, TransitoService ts, RefugioService rs, DonacionService ds, EmailServiceImpl es){
             this.adopcionService = as;
             this.voluntarioService = vs;
+            this.emailService = es;
             this.transitoService = ts;
             this.refugioService = rs;
             this.donacionService = ds;
@@ -250,6 +252,19 @@ public class PublicacionesApi {
         }
     }
 
+    @PostMapping("/adopciones/{id}/postular")
+    public ResponseEntity<?> postulacionAdopcion(@PathVariable Long id, @RequestBody FormularioDTO formularioDTO) {
+        Optional<Adopcion> oAdopcion = adopcionService.findById(id);
+        if (oAdopcion.isPresent()) {
+            if (emailService.sendMailDTO(formularioDTO, oAdopcion.get())) {
+                return ResponseEntity.ok().build();
+            } else {
+                return ResponseEntity.internalServerError().build();
+            }
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
     @GetMapping("/donaciones")
     public ResponseEntity<?> getAllDonaciones() {
         List<DonacionView> resultado = new ArrayList<>();
@@ -264,6 +279,35 @@ public class PublicacionesApi {
             return ResponseEntity.ok(DonacionView.toView(oDonacion.get()));
         }
         else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/transitos/{id}/postular")
+    public ResponseEntity<?> postulacionTransito(@PathVariable Long id, @RequestBody FormularioDTO formularioDTO) {
+        Optional<Transito> oTransito = transitoService.findById(id);
+        if (oTransito.isPresent()) {
+            if (emailService.sendMailDTO(formularioDTO,oTransito.get())){
+                return ResponseEntity.ok().build();
+            }else {
+                return ResponseEntity.internalServerError().build();
+            }
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/voluntariados/{id}/postular")
+    public ResponseEntity<?> postulacionVoluntariado(@PathVariable Long id, @RequestBody FormularioDTO formularioDTO) {
+        Optional<PublicacionVoluntariado> oVoluntariado = voluntarioService.findById(id);
+        if (oVoluntariado.isPresent()) {
+            if (emailService.sendMailDTO(formularioDTO, oVoluntariado.get())) {
+                return ResponseEntity.ok().build();
+            } else {
+                return ResponseEntity.internalServerError().build();
+            }
+        } else {
             return ResponseEntity.notFound().build();
         }
     }
