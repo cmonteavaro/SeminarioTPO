6402b11f543c343771d4d95f39ee0b64afe39e1e
VeintiTreinta <matisantoro10@gmail.com>
2022-11-01 17:40:24 -0300

    Cambios en About Us -> terminado

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -14,7 +14,7 @@ Como equipo, hemos decidido orientar nuestro proyecto en torno a los refugios de
 A lo largo del archivo, se podrá observar cómo fue el proceso de investigación y de recolección de datos mediante diferentes técnicas de relevamiento e ideación, tales como brainstorming, design thinking (con modelos como User Persona y mapas de empatía), entrevistas, encuestas, mapas de empatía entre otras. Buscamos encontrar las mayores debilidades y focos de dolor que afrontan tanto los refugios, como así también, aquellas personas interesadas en colaborar con ellos. 
 Con esto, planteamos la siguiente hipótesis: actualmente, hay grandes trabas y problemas de comunicación entre refugios y personas interesadas en colaborar, los cuales serán detallados en la siguiente sección: Problema.
 Detectamos mediante research propio y técnicas de relevamiento, que los refugios con grandes cantidades de seguidores o gran exposición en las redes sociales, no cuentan con la capacidad de poder responder a todas las consultas que reciben y, en la otra cara de la moneda, los refugios con menos exposición se ven ante la dificultad de poder llegar a una mayor cantidad de personas que puedan adoptar, transitar, donar o colaborar con ellos de alguna manera. 
- </p>
+        </p>
       </div>
     </div>
   );
