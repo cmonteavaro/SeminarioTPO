fc5d0ae03121d9e91565ec71197363c71ce2bcd3
latourfranco <latourfranco@gmail.com>
2022-11-01 11:00:54 -0300

    Seccion de about us --> imagen, titulo y texto (v1)

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,3 +1,16 @@
+import "../styles/about.css";
+import FotoAbout from "../images/AboutUsFotoFinal.png";
+
 export default function About() {
-  return <h1>Acerca de</h1>;
+  return (
+    <div className="aboutDiv">
+      <div>
+        <img src={FotoAbout} className="imagen"></img>
+      </div>
+      <div>
+        <h1>¿Quiénes somos?</h1>
+        <p>Hola somos unos genios. Gracias!</p>
+      </div>
+    </div>
+  );
 }
