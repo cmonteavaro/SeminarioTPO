3c61e2633cddee80d04d83501015dc3ead11829b
latourfranco <latourfranco@gmail.com>
2022-11-01 22:09:19 -0300

    Agregado de texto con formato

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -9,11 +9,14 @@ export default function About() {
       </div>
       <div className="acerca-de-detail">
         <h1 className="titulo">¿Quiénes somos?</h1>
-        <p className="parrafo">En el siguiente documento, se estará presentando el trabajo realizado durante los últimos meses en el marco de la materia Seminario de Integración Profesional I. 
-Como equipo, hemos decidido orientar nuestro proyecto en torno a los refugios de animales en Argentina. 
-A lo largo del archivo, se podrá observar cómo fue el proceso de investigación y de recolección de datos mediante diferentes técnicas de relevamiento e ideación, tales como brainstorming, design thinking (con modelos como User Persona y mapas de empatía), entrevistas, encuestas, mapas de empatía entre otras. Buscamos encontrar las mayores debilidades y focos de dolor que afrontan tanto los refugios, como así también, aquellas personas interesadas en colaborar con ellos. 
-Con esto, planteamos la siguiente hipótesis: actualmente, hay grandes trabas y problemas de comunicación entre refugios y personas interesadas en colaborar, los cuales serán detallados en la siguiente sección: Problema.
-Detectamos mediante research propio y técnicas de relevamiento, que los refugios con grandes cantidades de seguidores o gran exposición en las redes sociales, no cuentan con la capacidad de poder responder a todas las consultas que reciben y, en la otra cara de la moneda, los refugios con menos exposición se ven ante la dificultad de poder llegar a una mayor cantidad de personas que puedan adoptar, transitar, donar o colaborar con ellos de alguna manera. 
+        <p className="parrafo">
+          Nosotros, quienes desarrollamos esta plataforma, somos un grupo de estudiantes de informática apasionados por lo que hacemos, fans del trabajo que hacen los refugios animales y simpatizantes de aquellas personas que deciden colaborar con ellos de alguna manera.
+          Sabemos que los refugios, según el alcance en redes sociales, grandes <span className="palabraRemarcada">problemáticas de comunicación</span> en algunos casos y de <span className="palabraRemarcada">falta de llegada</span> en otros. Mientras que los potenciales adoptantes y colaboradores se pierden entre la <span className="palabraRemarcada">información descentralizada</span> y se les dificulta 
+          encontrar aquellos refugios y animales con los que serán compatibles.
+        </p>
+        <p className="parrafo">
+          No obstante, el crédito no es solo nuestro, sino también de todos los <span className="palabraRemarcada">refugios animales y colaboradores</span> que nos brindaron su tiempo para realizarles encuestas, entrevistas y observaciones. 
+          Esperamos que disfruten de este gran mejorar la vida de los pichichos.
         </p>
       </div>
     </div>
