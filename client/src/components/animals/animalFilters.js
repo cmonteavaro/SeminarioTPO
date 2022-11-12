f9cce6d1c7490276c0c90f532695c021bb280bb3
cmonteavaro <constantino20132@gmail.com>
2022-11-12 11:14:32 -0300

    termino los filtros

--- client/src/components/animals/animalFilters.js
+++ client/src/components/animals/animalFilters.js
@@ -4,34 +4,36 @@ import { Checkbox } from '@mantine/core';
 export default function AnimalFilter(){
 
     const [data, setData] = useState();
-    const [loading, setLoading] = useState(true);
+    const [filtersLoading, setLoading] = useState(true);
     const [filters, setFilters] = useState({}); 
-
-
-    useEffect(() => {
-        console.log(filters);
-    }, [filters]);
 
     useEffect(() => {
         setLoading(true);
         fetch(`http://localhost:8080/api/publicaciones/filtros`)
           .then((e) => e.json())
           .then((d) => {
-            setData(d);
-            data.tipoAnimal.map(filtro => setFilters(prevState => ({...prevState, [filtro]: false })))
-            data.tamanioAnimal.map(filtro => setFilters(prevState => ({...prevState, [filtro]: false })))
-            return null;
-          }).finally(() => setLoading(false))
+            setData(d)})     
+        .finally(() => setLoading(false))
       }, []);
     
+      useEffect(() => {
+        if (typeof data !== 'undefined'){
+            data.multivalores.TipoAnimalEnum.map(filtro => setFilters(prevState => ({...prevState, [filtro]: false })));
+            data.multivalores.TamanioEnum.map(filtro => setFilters(prevState => ({...prevState, [filtro]: false })));
+            data.booleanos.map(filtro => setFilters(prevState => ({...prevState, [filtro]: false })));
+            setFilters(prevState => ({...prevState, ['Es urgente']: false }));
+        }
+    }, [data]);
+
     function handleCheckboxToggle(event){
         const value = event.currentTarget.value;
-        console.log(value)
-        const checked = event.currentTarget.checked
-        setFilters(prevState => ({...prevState, `${value}`: checked }))    
+        const checked = event.currentTarget.checked;
+        let filtros = filters;
+        filtros[value] = checked;
+        setFilters(filtros);  
     }  
 
-    if (!loading) {
+    if (!filtersLoading) {
         return (
             <div className="filtersContainer">
                 <h1>Filtros</h1>
@@ -41,16 +43,23 @@ export default function AnimalFilter(){
                 </div>
                 <div className="filter-section">
                     <h4>Animal</h4>
-                    {data.tipoAnimal && data.tipoAnimal.length > 0 ? 
-                        data.tipoAnimal.map(filtro => <Checkbox value={filtro} label={filtro} 
+                    {data.multivalores.TipoAnimalEnum && data.multivalores.TipoAnimalEnum.length > 0 ? 
+                        data.multivalores.TipoAnimalEnum.map(filtro => <Checkbox value={filtro} label={filtro} 
                                                             onChange={event => handleCheckboxToggle(event)}/>) 
                         : null  
                     }
                 </div>
                 <div className="filter-section">
                     <h4>Tamaño</h4>
-                    {data.tamanioAnimal && data.tamanioAnimal.length > 0 ? 
-                        data.tamanioAnimal.map(filtro => <Checkbox value={filtro} label={filtro}
+                    {data.multivalores.TamanioEnum && data.multivalores.TamanioEnum.length > 0 ? 
+                        data.multivalores.TamanioEnum.map(filtro => <Checkbox value={filtro} label={filtro}
+                                                                onChange={event => handleCheckboxToggle(event)}/>) : null  
+                    }
+                </div>
+                <div className="filter-section">
+                    <h4>Extras</h4>
+                    {data.booleanos && data.booleanos.length > 0 ? 
+                        data.booleanos.map(filtro => <Checkbox value={filtro} label={filtro}
                                                                 onChange={event => handleCheckboxToggle(event)}/>) : null  
                     }
                 </div>
