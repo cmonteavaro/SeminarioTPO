86b52851e7027aa38cceba7011fc9f7b0e748a47
latourfranco <latourfranco@gmail.com>
2022-11-15 10:50:05 -0300

    Agregado de imagenes reales a los animales - Limpieza de transitos con refugio nulo

--- server/src/test/java/ar/edu/uade/server/ServerApplicationTests.java
+++ server/src/test/java/ar/edu/uade/server/ServerApplicationTests.java
@@ -57,6 +57,35 @@ class ServerApplicationTests {
     void statsAnimales() {
         List<Animal> allAnimales= animalService.findAll();
         System.out.println("Cantidad de Animales: "+allAnimales.size());
+        System.out.println("Cantidad de imagenes por animal: ");
+        allAnimales.forEach(a -> System.out.println(a.getId() + "-" + a.getNombre() + " (" + a.getTipoAnimal().getNombre() + "): " + a.getGaleriaImagenes().size()));
+    }
+
+    @Test
+    void buscarAnimal() {
+        Optional<Animal> oAnimal = animalService.findById(50L);
+        if (oAnimal.isPresent()) {
+            Animal animal = oAnimal.get();
+            System.out.println("Nombre: " + animal.getNombre());
+            System.out.println("Tamanio actual: " + animal.getTamanioActual().getNombre());
+            System.out.println("Tamanio esperado: " + animal.getTamanioEsperado().getNombre());
+            System.out.println("Edad: " + animal.getEdad());
+            System.out.println("Imagenes: ");
+            animal.getGaleriaImagenes().forEach(System.out::println);
+        }
+
+    }
+
+    @Test
+    void modificarAnimal() {
+        Optional<Animal> optionalAnimal = animalService.findById(50L);
+        if (optionalAnimal.isPresent()) {
+            Animal animal = optionalAnimal.get();
+            String imagen1 = "https://res.cloudinary.com/dypgglzvv/image/upload/v1668518955/Gato-blanco2_najjlp.jpg";
+            String imagen2 = "https://res.cloudinary.com/dypgglzvv/image/upload/v1668518955/Gato-blanco1_p3ipyr.jpg";
+            animal.agregarImagenes(imagen1, imagen2);
+            animalService.save(animal);
+        }
     }
 
     @Test
@@ -68,6 +97,7 @@ class ServerApplicationTests {
             System.out.println("Cantidad actual urgentes: "+r.getCantidadUrgentes());
             System.out.println("Cantidad maxima urgentes: "+(r.getPublicacionesAdopcion().size()+r.getPublicacionesTransito().size())*0.2);
             System.out.println("Pub adopcion: "+r.getPublicacionesAdopcion().size());
+            r.getPublicacionesAdopcion().forEach(a -> System.out.println((a == null ? "Nulo" : a.getId())));
             System.out.println("Pub transito: "+r.getPublicacionesTransito().size());
             System.out.println("Pub donacion: "+r.getPublicacionesDonacionesNoMonetarias().size());
             System.out.println("Pub voluntariado: "+r.getPublicacionesVoluntariado().size());
@@ -137,10 +167,40 @@ class ServerApplicationTests {
     }
 
     @Test
-    void agregarCorreoARefugios(){
-        for (Refugio r: refugioService.findAll()){
-            r.setCorreo("zoe.landeyro@gmail.com");
-            refugioService.save(r);
+    void getAllRefugios() {
+        List<Refugio> refugios = refugioService.findAll();
+        for (Refugio r: refugios) {
+            System.out.println("Direccion:" + r.getDireccion().getProvincia() + ", " + r.getDireccion().getCalle() + ", " + r.getDireccion().getLocalidad());
         }
+    }
+
+    @Test
+    void getAllAdopciones() {
+        List<Adopcion> adopciones = adopcionService.findAll();
+        for (Adopcion a: adopciones) {
+            System.out.println("Localidad refugio: " + a.getRefugio().getDireccion().getLocalidad());
+        }
+    }
+
+    @Test
+    void getAllTransitos() {
+        List<Transito> transitos = transitoService.findAll();
+        for (Transito t: transitos) {
+            System.out.println("ID: " + t.getId());
+            System.out.println("Animal: " + t.getAnimal().getNombre());
+            System.out.println("Localidad refugio: " + (t.getRefugio().getDireccion() == null ? "nulo" : t.getRefugio().getDireccion().getLocalidad()));
+        }
+    }
+
+    @Test
+    void eliminarTransitos() {
+        RepositoryODB repositoryODB = RepositoryODB.getInstancia();
+
+        List<Long> idsAEliminar = new ArrayList<>();
+        idsAEliminar.add(24L);
+        idsAEliminar.add(26L);
+        idsAEliminar.add(27L);
+        idsAEliminar.add(60L);
+        idsAEliminar.forEach(id -> repositoryODB.deleteById(Transito.class, id));
     }
 }
