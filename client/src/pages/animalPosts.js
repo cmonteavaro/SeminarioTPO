aa33762dfb4728d61282d01f6972d5af461e495f
Juan Dieguez <85458577+JuanDieguez@users.noreply.github.com>
2022-11-15 21:13:53 -0300

    Filters juampi (#73)
    * Adaptacion de views y endpoints. Creacion de funciones para filtros
    
    * Comportamiento de filtros
    
    * no se que poner
    
    * Filtros funcionales
    
    * Fix

--- client/src/pages/animalPosts.js
+++ client/src/pages/animalPosts.js
@@ -4,65 +4,83 @@ import { useEffect, useState } from "react";
 import { Loader } from "@mantine/core";
 import NotFound from "./notFound";
 import AnimalFilters from "../components/animals/animalFilters";
+import { applyFilters, init as filtersInit } from "../filters";
 
 export default function Posts() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [filtersJSON, setfiltersJSON] = useState();
-  const [filtersDict, setFiltersDict] = useState({});
+	const [dataDisplay, setDataDisplay] = useState([]);
+	const [dataFull, setDataFull] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [filtersJSON, setfiltersJSON] = useState();
+	const [filtersDict, setFiltersDict] = useState({});
 
-  // Get the data from the server
-  useEffect(() => {
-    setLoading(true);
-    Promise.all([
-      fetch(`http://localhost:8080/api/publicaciones/adopciones`),
-      fetch(`http://localhost:8080/api/publicaciones/filtros`),
-    ])
-      .then(([resAdopciones, resFiltros]) =>
-        Promise.all([resAdopciones.json(), resFiltros.json()])
-      )
-      .then(([dataAdopciones, dataFiltros]) => {
-        setData(dataAdopciones);
-        setfiltersJSON(dataFiltros);
-      })
-      .finally(() => setLoading(false));
-  }, []);
+	// Get the data from the server
+	useEffect(() => {
+		setLoading(true);
+		Promise.all([
+			fetch(`http://localhost:8080/api/publicaciones/adopciones`),
+			fetch(`http://localhost:8080/api/publicaciones/filtros`),
+			fetch("http://localhost:8080/api/publicaciones/adopciones/fullView"),
+		])
+			.then(([resAdopciones, resFiltros, resFullView]) =>
+				Promise.all([resAdopciones.json(), resFiltros.json(), resFullView.json()])
+			)
+			.then(([dataAdopciones, dataFiltros, dataFullView]) => {
+				setDataDisplay(dataAdopciones);
+				setDataFull(dataAdopciones);
+				setfiltersJSON(dataFiltros);
+				filtersInit(dataFullView);
+			})
+			.finally(() => setLoading(false));
+	}, []);
 
-  // Pass the filters into an array
-  useEffect(() => {
-    if (typeof filtersJSON !== "undefined") {
-      filtersJSON.multivalores.TipoAnimalEnum.map((filtro) =>
-        setFiltersDict((prevState) => ({ ...prevState, [filtro]: false }))
-      );
-      filtersJSON.multivalores.TamanioEnum.map((filtro) =>
-        setFiltersDict((prevState) => ({ ...prevState, [filtro]: false }))
-      );
-      filtersJSON.booleanos.map((filtro) =>
-        setFiltersDict((prevState) => ({ ...prevState, [filtro]: false }))
-      );
-      setFiltersDict((prevState) => ({ ...prevState, ["Es urgente"]: false }));
-    }
-  }, [data]);
+	// Pass the filters into an array
+	useEffect(() => {
+		if (typeof filtersJSON !== "undefined") {
+			filtersJSON.multivalores.TipoAnimalEnum.map((filtro) =>
+				setFiltersDict((prevState) => ({
+					...prevState,
+					[filtro]: false,
+				}))
+			);
+			filtersJSON.multivalores.TamanioEnum.map((filtro) =>
+				setFiltersDict((prevState) => ({
+					...prevState,
+					[filtro]: false,
+				}))
+			);
+			filtersJSON.booleanos.map((filtro) =>
+				setFiltersDict((prevState) => ({
+					...prevState,
+					[filtro]: false,
+				}))
+			);
+			setFiltersDict((prevState) => ({
+				...prevState,
+				["Es urgente"]: false,
+			}));
+		}
+	}, [filtersJSON]);
 
-  // Callback function called on AnimalFilters to change the state of the filter given
-  function handleCheckboxToggle(event) {
-    const value = event.currentTarget.value;
-    const checked = event.currentTarget.checked;
-    let filtros = filtersDict;
-    filtros[value] = checked;
-    setFiltersDict(filtros);
-  }
+	// Callback function called on AnimalFilters to change the state of the filter given
+	function handleCheckboxToggle(event) {
+		const value = event.currentTarget.value;
+		const checked = event.currentTarget.checked;
+		let filtros = filtersDict;
+		filtros[value] = checked;
+		setFiltersDict(filtros);
+		setDataDisplay(applyFilters([...dataFull], { ...filtersDict }));
+	}
 
-  if (loading) {
-    return (
-      <div className="loading">
-        <div className="loader">
-          <Loader color="lime" />
-        </div>
-        <h3>Estamos buscando a todos los animalitos para presentarte 😊</h3>
-      </div>
-    );
-  }
+	if (loading) {
+		return (
+			<div className="loading">
+				<div className="loader">
+					<Loader color="lime" />
+				</div>
+				<h3>Estamos buscando a todos los animalitos para presentarte 😊</h3>
+			</div>
+		);
+	}
 
   if (data.length < 1) {
     return <NotFound />;
@@ -78,7 +96,11 @@ export default function Posts() {
             />
           </section>
           <section className="cards">
-            <ListAnimals props={data} />
+            {dataDisplay.length < 1 ? (
+						<p> No se encontraron publicaciones para tu busqueda</p>
+					) : (
+						<ListAnimals props={dataDisplay} />
+					)}
           </section>
         </div>
       </>
