8c343ee92d2387c2c1333528c9aa8c569a081e41
Iara Denise Orosco <76779189+oroscoiara@users.noreply.github.com>
2022-11-28 23:26:53 -0300

    Correcciones textos de home page (#102)
    * modificaciones en textos de la home
    
    * acentos
    
    * correcciones home
    
    * se agranda fuente de titulos

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -64,13 +64,14 @@ export default function Home() {
             alt="Perro mirando a una pileta"
             src={Mila}
           />
-          <article className="info l">
-            <h2>Nuestra Motivacion</h2>
-            <p>
-              Nuestra Motivacion A lo largo de nuestra vida, hemos visto una
-              gran cantidad de animales que han sido abandonados o que no han
-              recibido amor, hoy en dia con las redes sociales hemos podidos
-              conectar con personas que tienen el fin de ayudarlos.
+          <article className="info r">
+          <h2 className="titulo">Nuestra motivación</h2>
+            <p className="parrafo">
+              A lo largo de nuestra vida, hemos visto una
+              enorme cifra de animales que han sido abandonados o que no han
+              recibido amor y, a su vez, una gran cantidad de refugios que no cuentan con la ayuda necesaria.
+              Hoy en día a través de las redes sociales hemos podido conectar con personas 
+              que tienen el fin de ayudarlos.
             </p>
           </article>
           <article className="background">
@@ -82,12 +83,14 @@ export default function Home() {
             <img src={House} />
           </article>
           <article className="info r">
-            <h2>MiRefugio</h2>
-            <p>
-              Nuestro fin es poder encontrar a todos los animales un transito o
-              un hogar. Conectamos refugios con adoptantes, transitantes y
-              colaboradores para poder darles una mejor vida a cada uno de
-              ellos.
+            <h2 className="titulo">Mi Refugio</h2>
+            <p className="parrafo">
+              Nuestro fin es poder facilitar la conexión entre las personas y aquellos animales que
+              en búsqueda de hogar o tránsito. 
+              Por esta razón, decidimos desarrollar un sitio 
+              donde las comunidades, refugios y voluntarios puedan conectar de una forma más 
+              fácil, rápida y centralizada, para poder darles una mejor calidad de vida a cada uno de
+              los animales.
             </p>
           </article>
           <img
@@ -102,18 +105,18 @@ export default function Home() {
             alt="Perro mirando a una pileta"
             src={Milo}
           />
-          <article className="info l">
-            <h2>Como funciona?</h2>
-            <p>
-              Si tienes intenciones de ayudar a algun refugio, puedes buscarlo
-              en la seccion de refugios. Pero si quieres transitar o adoptar
-              algun animal, puedes encontrarlos en la seccion de publicaciones.
+          <article className="info r">
+          <h2  className="titulo">Cómo funciona?</h2>
+            <p className="parrafo">
+             Si tenes intenciones de colaborar con algún refugio, podes encontrarlos
+              en la sección "Refugios". Si deseas transitar o adoptar
+              a algún animal, podrás encontrarlos en la sección "Publicaciones".
             </p>
           </article>
         </section>
         <section className="stats">
           <section className="column">
-            <h2>Al dia de hoy hemos logrado...</h2>
+            <h2>Al día de hoy hemos logrado...</h2>
             <article className="row">
               <div>
                 <img className="stats-img" src={HappyDog} />
@@ -121,7 +124,7 @@ export default function Home() {
               </div>
               <div>
                 <img className="stats-img" src={Transit} />
-                <p className="stats-info">+300 transitos</p>
+                <p className="stats-info">+300 tránsitos</p>
               </div>
             </article>
             <article className="row">
@@ -143,3 +146,4 @@ export default function Home() {
     </section>
   );
 }
+
